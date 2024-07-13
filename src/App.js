0e2afbb77b32a99bb1fbedc6291d445354db0e0b
ThoHung95 <ntlhungnuce@gmail.com>
2024-07-13 10:53:06 +0700

    update cursor

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import React, { Suspense, useState } from 'react';
-import { ChakraProvider, Fade } from '@chakra-ui/react';
-import { isMobile } from 'react-device-detect';
-import CursorImage from 'assets/media/cursor.png';
-import PointerImage from 'assets/media/pointer.png';
-import TextImage from 'assets/media/text.png';
+import React, { Suspense, useState } from "react";
+import { ChakraProvider, Fade } from "@chakra-ui/react";
+import { isMobile } from "react-device-detect";
+import CursorImage from "assets/media/Nyan_Cat_Cursor.png";
+import PointerImage from "assets/media/Nyan_Cat_Pointer.png";
+import TextImage from "assets/media/text.png";
 
-import './App.css';
-import Loading from 'components/Loading/Loading';
-import CustomCursor from 'components/CustomCursor/CustomCursor';
-const FullPage = React.lazy(() => import('components/FullPage/FullPage'));
+import "./App.css";
+import Loading from "components/Loading/Loading";
+import CustomCursor from "components/CustomCursor/CustomCursor";
+const FullPage = React.lazy(() => import("components/FullPage/FullPage"));
 
 function App() {
   const [isLoaded, changeIsLoaded] = useState(false);
-  if(isLoaded) {
+  if (isLoaded) {
     setTimeout(() => changeIsLoaded(true), 100);
   }
 
   return (
-    <Fade
-      in={!isLoaded}
-      unmountOnExit={true}
-    >
-      <ChakraProvider >
-        {
-          !isMobile && 
-          <CustomCursor 
-            defaultImg={CursorImage} 
-            pointerImg={PointerImage} 
-            textImg={TextImage} 
+    <Fade in={!isLoaded} unmountOnExit={true}>
+      <ChakraProvider>
+        {!isMobile && (
+          <CustomCursor
+            defaultImg={CursorImage}
+            pointerImg={PointerImage}
+            textImg={TextImage}
           />
-        }
+        )}
         <div className="App">
           <Suspense fallback={<Loading />}>
             <FullPage />
